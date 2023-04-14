dda595e84f21dbf595c9f00784698ac420a5501f
Colorathe <joacovulcano@gmail.com>
2023-04-14 11:24:55 -0300

    modified: add text for my name

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -4,14 +4,13 @@ function Intro() {
   return (
     <div className='flex items-center justify-center flex-col text-center pt-20 pb-12'>
       <h className='text-4xl md:text-7x1 mb-1 md:mb-3 font-bold dark:text-white'>
-        Joaquin
+        Joaquin Vulcano
       </h>
       <p className='text-base md:text-xl mb-3 font-med'>Full Stack Developer</p>
       <p className='text-sm max-w-xl mb-6 font-bold'>
         Meet Joaquin, a Junior Full-Stack Developer with a passion for creating
         captivating and intuitive web applications. With a strong foundation in
-        HTML, CSS, JavaScript, React, C# and .NET. Joaquin leverages their
-        technical expertise to bring innovative ideas to life.
+        HTML, CSS, JavaScript, React, C# and .NET.
         <br />
         Driven by a love for problem-solving, Joaquin is constantly seeking new
         challenges and opportunities to expand their skillset. Their passion for
