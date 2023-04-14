f57db1eb58680a20b920c35fdef76b08014e8515
Colorathe <joacovulcano@gmail.com>
2023-04-14 11:57:29 -0300

    modified

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,37 +1,43 @@
 export default [
   {
     title: "Error 404 Page",
-    imgUrl: "src/assets/error404-page.png",
+    imgUrl:
+      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/error404-page.png",
     stack: ["HTML", "CSS"],
     link: "https://error404-page-colorathe.netlify.app/",
   },
   {
     title: "Login Page",
-    imgUrl: "src/assets/login-page.png",
+    imgUrl:
+      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/login-page.png",
     stack: ["React", "Tailwind"],
     link: "https://login-page-colorathe.netlify.app/",
   },
   {
     title: "Veterinaria Dante Administration",
-    imgUrl: "src/assets/veterinaria-admin.png",
+    imgUrl:
+      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/veterinaria-admin.png",
     stack: ["React", "JavaScript", "Tailwind"],
     link: "https://citas-veterinaria-dante.netlify.app/",
   },
   {
     title: "Crypto App",
-    imgUrl: "src/assets/crypto-app.png",
+    imgUrl:
+      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/crypto-app.png",
     stack: ["React", "CSS"],
     link: "https://cotizador-criptomonedas2-reactjs.netlify.app/",
   },
   {
     title: "Gift Search",
-    imgUrl: "src/assets/gift-page.png",
+    imgUrl:
+      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/gift-page.png",
     stack: ["React", "CSS"],
     link: "https://buscador-gifs-joaquin.netlify.app/",
   },
   {
     title: "To-Do App",
-    imgUrl: "src/assets/veterinaria-admin.png",
+    imgUrl:
+      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/to-do-app.png",
     stack: ["React", "Tailwind"],
     link: "https://to-do-app-colorathe.netlify.app/",
   },
