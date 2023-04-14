3e037b004618892eda828dd4b1738f1a74aee81f
Colorathe <joacovulcano@gmail.com>
2023-04-14 13:04:42 -0300

    modified

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,42 +2,42 @@ export default [
   {
     title: "Error 404 Page",
     imgUrl:
-      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/error404-page.png",
+      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/error404-page.png?raw=true",
     stack: ["HTML", "CSS"],
     link: "https://error404-page-colorathe.netlify.app/",
   },
   {
     title: "Login Page",
     imgUrl:
-      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/login-page.png",
+      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/login-page.png?raw=true",
     stack: ["React", "Tailwind"],
     link: "https://login-page-colorathe.netlify.app/",
   },
   {
     title: "Veterinaria Dante Administration",
     imgUrl:
-      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/veterinaria-admin.png",
+      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/veterinaria-admin.png?raw=true",
     stack: ["React", "JavaScript", "Tailwind"],
     link: "https://citas-veterinaria-dante.netlify.app/",
   },
   {
     title: "Crypto App",
     imgUrl:
-      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/crypto-app.png",
+      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/crypto-app.png?raw=true",
     stack: ["React", "CSS"],
     link: "https://cotizador-criptomonedas2-reactjs.netlify.app/",
   },
   {
     title: "Gift Search",
     imgUrl:
-      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/gift-page.png",
+      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/gift-page.png?raw=true",
     stack: ["React", "CSS"],
     link: "https://buscador-gifs-joaquin.netlify.app/",
   },
   {
     title: "To-Do App",
     imgUrl:
-      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/to-do-app.png",
+      "https://github.com/Colorathe/portfolio-react-last/blob/master/src/assets/to-do-app.png?raw=true",
     stack: ["React", "Tailwind"],
     link: "https://to-do-app-colorathe.netlify.app/",
   },
