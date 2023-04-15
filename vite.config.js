33a27885dbf45c38c1677dc5f246bfbb9fc76289
Colorathe <joacovulcano@gmail.com>
2023-04-15 16:50:41 -0300

    modified

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-react-last/",
+  base: "https://Colorathe.github.io/portfolio-react-last/",
 });
