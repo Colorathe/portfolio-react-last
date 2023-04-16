cc56838bb9ab77eeb5bdc34d76edc05b4d6a436e
Colorathe <joacovulcano@gmail.com>
2023-04-16 19:22:03 -0300

    modified

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -4,27 +4,27 @@ export default [
     title: "Full Stack Developer at Freelancer",
     duration: "Actually",
     details:
-      "As a full-stack developer with experience in C#, .NET, and React, you will be responsible for designing and developing complex software solutions for the company. This may involve working on both the front-end and back-end of web applications, creating and maintaining databases, and integrating with third-party APIs.",
+      "As a full-stack developer with experience in C#, .NET, and React, I will be responsible for designing and developing complex software solutions for the company. This may involve working on both the front-end and back-end of web applications, creating and maintaining databases, and integrating with third-party APIs.",
   },
   {
     year: "2022",
     title: "Representative at Konecta",
     duration: "7 months",
     details:
-      "As a customer service representative, you will be responsible for providing excellent customer support to clients of the company. This may involve handling inquiries and complaints, resolving issues, and processing orders and returns.",
+      "As a customer service representative, was be responsible for providing excellent customer support to clients of the company. This may involve handling inquiries and complaints, resolving issues, and processing orders and returns.",
   },
   {
     year: "2021",
     title: "Administrative",
     duration: "8 months",
     details:
-      "As an administrative at a beer company, your daily responsibilities may include managing office operations, organizing meetings, and maintaining company records. You may also be responsible for processing orders and invoices, monitoring inventory levels, and coordinating with suppliers and vendors. ",
+      "As an administrative at a beer company, my daily responsibilities may include managing office operations, organizing meetings, and maintaining company records. i was also be responsible for processing orders and invoices, monitoring inventory levels, and coordinating with suppliers and vendors.",
   },
   {
     year: "2020",
     title: "System Ingineer",
     duration: "Actually",
     details:
-      "You are currently in your second year of the Systems Engineering program, which focuses on designing and developing complex computer-based systems. This program combines principles from computer science, mathematics, and engineering to create solutions for real-world problems.",
+      "I'm currently in your second year of the Systems Engineering program, which focuses on designing and developing complex computer-based systems. This program combines principles from computer science, mathematics, and engineering to create solutions for real-world problems.",
   },
 ];
